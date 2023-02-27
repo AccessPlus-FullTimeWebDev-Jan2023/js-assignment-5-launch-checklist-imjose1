61b91497222bc3dab30e02100c192d7ab0bcf91f
imjose1 <jose_ghj@hotmail.com>
2023-02-26 16:29:35 -0800

    fix letters

--- scriptHelper.js
+++ scriptHelper.js
@@ -58,12 +58,12 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
                 cargoStatus.innerHTML=`Cargo mass too heavy for launch`;
             }else if(fuelLevel < 10000 && cargoLevel > 10000){
                 launchStatus.style.color ="#C7254E";
-                launchStatus.innerHTML =`Shuttle Not Ready for launch`;
+                launchStatus.innerHTML =`Shuttle Not Ready for Launch`;
                 fuelStatus.innerHTML =`Fuel level too low for launch`;
                 cargoStatus.innerHTML=`Cargo mass too heavy for launch`;
             }else{
                 launchStatus.style.color ="#419F6A";
-                launchStatus.innerHTML =`Shuttle is Ready for launch`;
+                launchStatus.innerHTML =`Shuttle is Ready for Launch`;
                 fuelStatus.innerHTML =`Fuel level high enough for launch`;
                 cargoStatus.innerHTML =`Cargo mass low enough for launch`;
             }
